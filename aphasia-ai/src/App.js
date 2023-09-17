36f8e8a9e700a73a702003e47e646e2dc6019f04
Joshua Wang <wangjoshua8@gmail.com>
2023-09-17 15:00:57 +1000

    Delete unnecessary search bar files.

--- aphasia-ai/src/App.js
+++ aphasia-ai/src/App.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { SearchBar } from './components/SearchBar';
-import { SearchResultOutput } from './components/SearchResultOutput';
 import './App.css';
 import FileUploader from './components/FileUploader';
 import { FileUploadOutput } from './components/FileUploadOutput';
@@ -8,24 +6,9 @@ import Logo from './assets/apha-ai-logo.png';
 
 function App()
 {
-    // const [searchComplete, setSearchComplete] = useState(false);
-    // const [searchTerm, setSearchTerm] = useState('');
-    // const [searchResults, setSearchResults] = useState([]);
     const [blobVisible, setBlobVisible] = useState(false);
     const [fileResults, setFileResults] = useState([]);
     const [filename, setFilename] = useState('');
-
-    // const handleSearch = (searchTerm) =>
-    // {
-    //     setSearchTerm(searchTerm);
-    //     setSearchResults([]); // clear previous search results            
-    // }
-
-    // const handleSearchComplete = (results) =>
-    // {
-    //     setSearchResults(results);
-    //     setBlobVisible(results.length !== 0 ? true : false);
-    // }
 
     const handleProcessingComplete = (filename, results) =>
     {
@@ -38,8 +21,6 @@ function App()
         <div className="App">
             <header className="App-header">
                 <img src={Logo} alt="Logo" className="App-logo" width='400' style={{padding: '40px'}}/>
-                {/* <SearchBar onSearch={handleSearch} onSearchComplete={handleSearchComplete} />
-                <SearchResultOutput searchTerm={searchTerm} results={searchResults} isVisible={blobVisible} /> */}
                 <FileUploader onProcessingComplete={handleProcessingComplete}/>
                 <FileUploadOutput filename={filename} results={fileResults} isVisible={blobVisible}/>
             </header>
