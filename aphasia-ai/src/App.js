12e9c63bcc80b7b5cfe56aa498b61e7e9882cef9
Virat Chaudhary <v.chaudhary@uqconnect.edu.au>
2023-10-02 21:41:46 +1000

    added bert model api integration

--- aphasia-ai/src/App.js
+++ aphasia-ai/src/App.js
@@ -20,11 +20,24 @@ function App() {
         return () => clearTimeout(loadingTimeout);
     }, []);
 
-    const handleProcessingComplete = (filename, results) =>
-    {
+    const handleProcessingComplete = (filename, results) => {
         setFileResults(results);
         setFilename(filename);
         setBlobVisible(results.length !== 0 ? true : false);
+    }
+
+    async function query() {
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/VCInit/autotrain-aphasia-simplification-92527144747",
+            {
+                headers: { Authorization: "Bearer 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" },
+                method: "POST",
+                body: JSON.stringify({ "inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct." }),
+            }
+        );
+        const result = await response.json();
+        console.log(result[0].summary_text);
+        return result;
     }
 
     return (
@@ -36,6 +49,7 @@ function App() {
             ) : (
                 <header className="App-header">
                     <img src={Logo} alt="Logo" className="App-logo" width='400' style={{ padding: '40px' }} />
+                    <button onClick={query}> Generate Response</button>
                     <FileUploader onProcessingComplete={handleProcessingComplete} />
                     <FileUploadOutput filename={filename} results={fileResults} isVisible={blobVisible} />
                 </header>
