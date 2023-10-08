0a322ce3ce1c4ef0a2b97c44e407c23317d001ba
Virat Chaudhary <v.chaudhary@uqconnect.edu.au>
2023-10-08 17:39:43 +1000

    conditional rendering for speech buttons

--- aphasia-ai/src/components/FileUploadOutput.js
+++ aphasia-ai/src/components/FileUploadOutput.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './FileUploadOutput.css'
 
 export const FileUploadOutput = ({ filename, results, isVisible }) => {
     const [readResults, setReadResults] = useState(false);
+    const [newSpeech, setNewSpeech] = useState(true);
 
     const resetSpeech = () => {
         speechSynthesis.cancel();
         setReadResults(false);
+        setNewSpeech(true);
     }
 
     const pauseSpeech = () => {
@@ -14,26 +16,38 @@ export const FileUploadOutput = ({ filename, results, isVisible }) => {
         setReadResults(false);
     }
 
-    const resumeSpeech = () => {
-        speechSynthesis.resume();
-        setReadResults(true);
-    }
+    const startSpeech = (newSpeech) => {
+        if (newSpeech) {
+            const resultsText = results.join('. '); // Combine results into a single string
+            const utterance = new SpeechSynthesisUtterance(`File results for ${filename}: ${resultsText}`);
+            utterance.rate = 0.75;
 
-    const startSpeech = () => {
-        const resultsText = results.join('. '); // Combine results into a single string
-        const utterance = new SpeechSynthesisUtterance(`File results for ${filename}: ${resultsText}`);
-        utterance.rate = 0.75;
-
-        setReadResults(true);
-        speechSynthesis.speak(utterance);
+            speechSynthesis.speak(utterance);
+            setReadResults(true);
+            setNewSpeech(false);
+        }
+        else {
+            speechSynthesis.resume();
+            setReadResults(true);
+        }
     }
 
     return (
         <div className={`file-output-blob ${isVisible ? 'visible' : ''}`}>
-            <button aria-label="Read results" className="sr-only" onClick={startSpeech}> Read Results </button>
-            <button onClick={resumeSpeech}>Unpause</button>
-            <button onClick={pauseSpeech}>Pause</button>
-            <button onClick={resetSpeech}>Reset</button>
+            {!readResults ? (
+                <>
+                    {newSpeech ? (
+                        <button aria-label="Read results" className="sr-only" onClick={() => startSpeech(true)}> Read Results </button>
+                    ) : (
+                        <button onClick={() => startSpeech(false)}> Unpause </button>
+                    )}
+                </>
+            ) : (
+                <>
+                    <button onClick={pauseSpeech}>Pause</button>
+                    <button onClick={resetSpeech}>Reset</button>
+                </>
+            )}
 
             {isVisible &&
                 <div>
