2fc25820205025c9b1e16f93a66067502feacbcd
pranav2087 <pranavprakash456@gmail.com>
2023-10-11 16:18:19 +1000

    made the meaning showup in the fileuploadoutput

--- aphasia-ai/src/App.js
+++ aphasia-ai/src/App.js
@@ -4,8 +4,6 @@ import FileUploader from './components/FileUploader';
 import { FileUploadOutput } from './components/FileUploadOutput';
 import { LoadingScreen } from './components/LoadingScreen';
 import Logo from './assets/apha-ai-logo.png';
-import Axios from "axios";
-import Popup from "./components/PopUp";
 import { Instructions } from './components/Instructions';
 
 function App() {
@@ -14,10 +12,6 @@ function App() {
     const [filename, setFilename] = useState('');
     const [isLoading, setIsLoading] = useState(true);
     const [keywords, setKeywords] = useState([]);
-    const [data, setData] = useState(null);
-    const [selectedWord, setSelectedWord] = useState("");
-    const [contextMenuVisible, setContextMenuVisible] = useState(false);
-    const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
 
@@ -34,59 +28,8 @@ function App() {
         setBlobVisible(results.length !== 0 ? true : false);
         setKeywords(keywords);
     }
-
-    useEffect(() => {
-        const fetchData = async () => {
-            if (selectedWord && contextMenuVisible) {
-                try {
-                    const response = await Axios.get(
-                        `https://api.dictionaryapi.dev/api/v2/entries/en_US/${selectedWord.toLowerCase()}`
-                    );
-                    setData(response.data[0]);
-                } catch (error) {
-                    console.error("Error fetching data:", error);
-                }
-            }
-        };
-
-        fetchData();
-    }, [selectedWord, contextMenuVisible]);
-
-    const handleContextMenu = (e) => {
-        e.preventDefault();
-        const selectedText = window.getSelection().toString();
-        if (selectedText) {
-            setSelectedWord(selectedText);
-            setContextMenuPosition({ x: e.clientX, y: e.clientY });
-            setContextMenuVisible(true);
-        } else {
-            setContextMenuVisible(false);
-        }
-    };
-
-    const handleClosePopup = () => {
-        setContextMenuVisible(false);
-    };
-
-    // Add a click event listener to the document to close the popup when clicking outside
-    useEffect(() => {
-        const handleDocumentClick = (e) => {
-            if (contextMenuVisible) {
-                const popupElement = document.querySelector(".popup");
-                if (popupElement && !popupElement.contains(e.target)) {
-                    handleClosePopup();
-                }
-            }
-        };
-
-        document.addEventListener("click", handleDocumentClick);
-
-        return () => {
-            document.removeEventListener("click", handleDocumentClick);
-        };
-    }, [contextMenuVisible]);
     return (
-        <div className="App" onContextMenu={handleContextMenu}>
+        <div className="App">
             {isLoading ? (
                 <header className='Loading-screen'>
                     <LoadingScreen />
@@ -105,9 +48,7 @@ function App() {
                     </footer>
                 </header>
             )}
-            {contextMenuVisible && (
-                <Popup data={data} position={contextMenuPosition} />
-            )}
+
         </div>
     );
 }
