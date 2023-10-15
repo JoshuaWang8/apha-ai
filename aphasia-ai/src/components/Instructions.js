09b21aaeba91ad295b7a71fd53defe0046afe159
Virat Chaudhary <v.chaudhary@uqconnect.edu.au>
2023-10-15 15:34:08 +1000

    Fixed output window styling

--- aphasia-ai/src/components/Instructions.js
+++ aphasia-ai/src/components/Instructions.js
@@ -9,30 +9,32 @@ import './Instructions.css'
 export const Instructions = () => {
     return (
         <Template>
-            <div className='instruction-section-heading'>
-                <h1> How it Works? </h1>
-            </div>
-            <div className="instructions-container">
-                <div className="upload-instruction">
-                    <h2>
-                        Upload a file
-                    </h2>
-                    <LuUpload size={200} color='navy' />
+            <div className='instruction-page'>
+                <div className='instruction-section-heading'>
+                    <h1> How it Works? </h1>
                 </div>
-                <LuArrowRight color='white' />
-                <div className="summarise-instruction">
-                    <h2>
-                        We Summarise the file
-                    </h2>
-                    <LuFileCog2 size={200} color='navy' />
+                <div className="instructions-container">
+                    <div className="upload-instruction">
+                        <h2>
+                            Upload a file
+                        </h2>
+                        <LuUpload size={200} color='navy' />
+                    </div>
+                    <LuArrowRight color='white' />
+                    <div className="summarise-instruction">
+                        <h2>
+                            We Summarise the file
+                        </h2>
+                        <LuFileCog2 size={200} color='navy' />
 
-                </div>
-                <LuArrowRight color='white' />
-                <div className="output-instruction">
-                    <h2>
-                        View Output
-                    </h2>
-                    <LuFileText size={200} color='navy' />
+                    </div>
+                    <LuArrowRight color='white' />
+                    <div className="output-instruction">
+                        <h2>
+                            View Output
+                        </h2>
+                        <LuFileText size={200} color='navy' />
+                    </div>
                 </div>
             </div>
         </Template>
